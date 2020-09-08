186571bf6795cc24a93450596c71cc7c5be89433
zeero1q <shrikantvishwakrma74@gmail.com>
2020-09-08 07:14:04 +0530

    Bootstrap class added. closes #15

---
+++ src/main/java/com/shri/zeero/bootstrap/DataLoader.java
@@ -0,0 +1,57 @@
+package com.shri.zeero.bootstrap;
+
+import com.shri.zeero.domain.Beer;
+import com.shri.zeero.repository.BeerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by zeeroiq on 9/8/20, 5:17 AM
+ */
+
+@Component
+public class DataLoader implements CommandLineRunner {
+
+    private final BeerRepository repository;
+
+    public DataLoader(BeerRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        loadObjects();
+    }
+
+    private void loadObjects() {
+
+        if(repository.count() == 0) {
+            repository.save(Beer.builder()
+                    .beerName("CORONA")
+                    .beerStyle("PALE_LAGER")
+                    .quantityToBrew(200)
+                    .minOnHand(20)
+                    .upc(32321232132L)
+                    .price(new BigDecimal("120.00"))
+                    .build());
+            repository.save(Beer.builder()
+                    .beerName("BIRA")
+                    .beerStyle("LAGER")
+                    .quantityToBrew(100)
+                    .minOnHand(25)
+                    .upc(323212343132L)
+                    .price(new BigDecimal("130.00"))
+                    .build());
+            repository.save(Beer.builder()
+                    .beerName("TUBORG")
+                    .beerStyle("MALT")
+                    .quantityToBrew(10)
+                    .minOnHand(20)
+                    .upc(323212343132L)
+                    .price(new BigDecimal("190.00"))
+                    .build());
+        }
+    }
+}
